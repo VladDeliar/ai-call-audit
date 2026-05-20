18090147eaeb3c790ef235da673b8d4f07f57714
VladDeliar <vladd1331@gmail.com>
2026-05-20 22:40:06 +0300

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print(torch.__version__)                  # має бути типу 2.x.x+cu128
-print(torch.cuda.is_available())          # True
-print(torch.cuda.get_device_name(0))      # NVIDIA GeForce GTX 1050 Ti
-print(torch.version.cuda)                 # 12.8
